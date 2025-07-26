710e3d99e914fc88c514e442a08fb6bc642267e8
Nandani Sinha <nandanisinha2504@gmail.com>
2025-07-26 16:21:04 +0530

    i am solving a hard question

---
+++ Base/too.java
@@ -0,0 +1,11 @@
+package Base;
+
+public class too {
+    public long maxSubarrays(int n, int[][] conflictingPairs) {
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
